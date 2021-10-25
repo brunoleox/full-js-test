f295f37b59dea37b8a9640fe97a9ac9592dc1a3e
Bruno <bruno_leonardosilva@hotmail.com>
2021-10-25 11:33:58 -0400

    Criação de uma rota Home

--- frontend/src/routes/routes.js
+++ frontend/src/routes/routes.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Route, BrowserRouter } from "react-router-dom";
 
+import Home from "../pages/home";
 import Quotes from "../pages/quotes";
 import Historic from "../pages/historic";
 import Gains from "../pages/gains";
@@ -9,7 +10,8 @@ import Compare from "../pages/compare";
 const Routes = () => {
    return(
        <BrowserRouter>
-           <Route component = { Quotes }  path="/" exact />
+           <Route component = { Home }  path="/" exact />
+           <Route component = { Quotes }  path="/quotes" />
            <Route component = { Historic }  path="/historic" />
            <Route component = { Gains }  path="/gains" />
            <Route component = { Compare }  path="/comapare" />
