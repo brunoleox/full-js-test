77d404fe3ad405c58533d376be2f9e38586de9af
Bruno <bruno_leonardosilva@hotmail.com>
2021-10-24 20:37:28 -0400

    Criação de props no componente Input

--- frontend/src/components/input/index.jsx
+++ frontend/src/components/input/index.jsx
@@ -1,7 +1,15 @@
 import { AreaText } from "./styles";
 
 const Input = (props) => {
-  return <AreaText placeholder={props.placeholder}></AreaText>;
+  return <AreaText 
+  placeholder={props.placeholder} 
+  onChange={props.onChange}
+  type={props.type}
+  id={props.id}
+  name={props.name}
+  >
+
+  </AreaText>;
 };
 
 export default Input;
