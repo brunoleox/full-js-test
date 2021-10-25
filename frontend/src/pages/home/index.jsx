1704c51541e60246a9847bdb3bace042d6e10f91
Bruno <bruno_leonardosilva@hotmail.com>
2021-10-25 11:34:25 -0400

    Criação pagina home

---
+++ frontend/src/pages/home/index.jsx
@@ -0,0 +1,12 @@
+import Title from "../../components/title";
+
+const Home = () => {
+  return (
+    <Title
+      title="Seja bem vindo!"
+      subtitle="Navegue pelos menus laterais para obter dados de ações de empresas."
+    ></Title>
+  );
+};
+
+export default Home;
